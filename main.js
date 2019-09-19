7907d3a59de0158e5f42e778bb7539b9ad18ab78
Nelson Trujillo <nelsontrujillog@gmail.com>
2019-09-19 10:43:10 -0500

    NTR-feat:Add require

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 'use strict'
 
+require.config({
+    paths: {
+        "angular": "/libraries/angular/angular",       
+        "kendo.all.min": "/libraries/kendo/kendo.all.min"
+    }
+});
 
-var app = angular.module("my-app", ["kendo.directives"]);
+require(['angular', 'kendo.all.min'], function () {
 
-app.controller("MyCtrl", ["$scope", function ($scope) {
-    $scope.birthday = new Date();
-    $scope.phone = "555 123 4567";
-    $scope.cc = "1234 1234 1234 1234"
-    $scope.ssn = "003-12-3456";
-    $scope.post = "W1N 1AC";
-}]);
+    var app = angular.module("my-app", ["kendo.directives"]);
 
-angular.bootstrap(document.getElementById("my-element"), ["my-app"]);
+    app.controller("MyCtrl", ["$scope", function ($scope) {
+        $scope.birthday = new Date();
+        $scope.phone = "555 123 4567";
+        $scope.cc = "1234 1234 1234 1234"
+        $scope.ssn = "003-12-3456";
+        $scope.post = "W1N 1AC";
+    }]);
+
+    angular.bootstrap(document.getElementById("my-element"), ["my-app"]);
+}, function (err) { console.log(err) })
+
+
